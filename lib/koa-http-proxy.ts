09e31fd0f5be7be7232002c5c04817e950df29f7
Erwin Villejo <erwin.villejo@gmail.com>
2020-04-28 22:32:10 +0800

    stream only, don't buffer

--- lib/koa-http-proxy.ts
+++ lib/koa-http-proxy.ts
@@ -4,7 +4,7 @@ import { PassThrough } from 'stream'
 import { Middleware, Response } from 'koa'
 import { createProxy } from 'http-proxy'
 
-function makeProxyResponseAdapter(response: Response, done: (v?: unknown) => void, bufferResponse: boolean = true): ServerResponse {
+function makeProxyResponseAdapter(response: Response, done: (v?: unknown) => void): ServerResponse {
   const resAdapter: ServerResponse = new OutgoingMessage() as any
 
   resAdapter.on('pipe', (proxyRes) => {
@@ -29,37 +29,21 @@ function makeProxyResponseAdapter(response: Response, done: (v?: unknown) => voi
       }
     }
 
-    // buffer or stream body
-    if (bufferResponse) {
-      const chunks: Buffer[] = []
-      proxyRes.on('data', chunk => chunks.push(chunk))
-      proxyRes.on('end', () => {
-        let resContentLength = resAdapter.getHeader('Content-Length')
-        if (typeof resContentLength === 'string') {
-          resContentLength = parseInt(resContentLength)
-        }
-        if (Array.isArray(resContentLength) || Number.isNaN(resContentLength ?? NaN)) {
-          resContentLength = undefined
-        }
-        response.body = Buffer.concat(chunks,  resContentLength)
-        done()
-      })
-    } else {
-      response.body = proxyRes.pipe(new PassThrough())
-      done()
-    }
+    // stream body
+    response.body = proxyRes.pipe(new PassThrough())
+    done()
   })
 
   return resAdapter
 }
 
-export default function(target: URL, bufferResponse: boolean = true): Middleware {
+export default function(target: URL): Middleware {
   const proxy = createProxy({ target })
 
   return async(ctx, next) => {
     await new Promise((resolve, reject) => {
-      const resAdapter = makeProxyResponseAdapter(ctx.response, resolve, bufferResponse)
-      proxy.web(ctx.req, resAdapter, undefined, reject)
+      const resAdapter = makeProxyResponseAdapter(ctx.response, resolve)
+      proxy.web(ctx.req, resAdapter, reject)
     })
 
     return next()
